33a864afb3194e326b7e29f497d81adbaed6221b
Prokop <prokop.schield@protonmail.com>
2022-12-29 17:55:45 +0100

    feat(src/helpers.ts): add error embedder

---
+++ src/helpers.ts
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import { sanitize } from 'isomorphic-dompurify';
+
+export const modifyIndex = (error: string) => {
+	const index = fs.readFileSync(__dirname + '/index.html', 'utf-8');
+
+	return index.replace(
+		'{{ ERROR }}',
+		error ? `<blockquote>${sanitize(error)}</blockquote>` : ''
+	);
+};
+
+export const noErrorIndex = modifyIndex('');
