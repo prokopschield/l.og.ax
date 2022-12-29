8e5d2e9ea0c77ad8998c62a4c01ff02a9e03e66e
Prokop <prokop.schield@protonmail.com>
2022-12-29 17:56:05 +0100

    feat(src/server.ts): implement homepage

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { listen } from 'nodesite.eu-local';
 
 import { domain, port } from './constants';
 import { query, ready } from './db';
+import { modifyIndex, noErrorIndex } from './helpers';
 import { shorten } from './shortener';
 
 export const init = async () => {
@@ -13,13 +14,24 @@ export const init = async () => {
 
 	await ready;
 
+	const ROOT_URL = new URL(`https://${domain}`);
+
 	create('/', async (request) => {
 		try {
 			const fixed_url = request.uri
 				.replace(/(https?)\:\/+([^\/])/, '$1://$2')
 				.slice(1);
 
-			const { pathname } = new URL(request.uri, 'a://b');
+			const { pathname } = new URL(request.uri, ROOT_URL);
+
+			if (pathname === '/') {
+				return {
+					statusCode: 302,
+					head: {
+						Location: '/homepage/',
+					},
+				};
+			}
 
 			let key = pathname.slice(1).toLowerCase();
 
@@ -59,6 +71,63 @@ export const init = async () => {
 			return {
 				statusCode: 400,
 				body: String(error),
+			};
+		}
+	});
+
+	create('/homepage', async (request) => {
+		const { searchParams } = new URL(request.uri, ROOT_URL);
+
+		const error = searchParams.get('error') || '';
+
+		const html = Buffer.from(error ? modifyIndex(error) : noErrorIndex);
+
+		return {
+			statusCode: 200,
+			head: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Methods': '*',
+				'Access-Control-Allow-Headers': '*',
+				'Content-Type': 'text/html',
+				'Content-Length': String(html.length),
+			},
+			body: html,
+		};
+	});
+
+	create('/new.php', async (request) => {
+		let url = '';
+
+		try {
+			const { searchParams } = new URL(request.uri, ROOT_URL);
+
+			const url_object = new URL(searchParams.get('url') || '');
+
+			url = url_object.href;
+
+			if (url_object.host !== domain) {
+				url = await shorten(url);
+			}
+
+			return {
+				statusCode: 200,
+				head: {
+					'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Methods': '*',
+					'Access-Control-Allow-Headers': '*',
+					'Content-Type': 'text/plain',
+				},
+				body: url,
+			};
+		} catch (error) {
+			const redirect_url = new URL('/homepage/error', ROOT_URL);
+
+			redirect_url.searchParams.set('error', String(error));
+			redirect_url.searchParams.set('url', url);
+
+			return {
+				statusCode: 302,
+				head: { Location: redirect_url.href },
 			};
 		}
 	});
