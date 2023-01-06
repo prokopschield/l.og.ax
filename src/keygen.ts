8071b91a91ac800374b0cd6d8c0686b181d0be69
Prokop <prokop.schield@protonmail.com>
2023-01-06 21:48:00 +0100

    fix(src/shortener.ts): prevent duplicate keys

---
+++ src/keygen.ts
@@ -0,0 +1,16 @@
+import { KEY_LENGTH, chars } from './constants';
+import { query } from './db';
+
+export async function generateKey() {
+	do {
+		const key = [...Array(KEY_LENGTH).keys()]
+			.map(() => chars[Math.floor(Math.random() * 1024) % chars.length])
+			.join('');
+
+		if (await query('short_link')(key)) {
+			continue;
+		} else {
+			return key;
+		}
+	} while (true);
+}
--- src/shortener.ts
+++ src/shortener.ts
@@ -1,7 +1,8 @@
 import { cacheFn, Queue } from 'ps-std';
 
-import { chars, domain, KEY_LENGTH } from './constants';
+import { domain } from './constants';
 import { addRow, query } from './db';
+import { generateKey } from './keygen';
 
 const queue = new Queue(console.error);
 
@@ -14,9 +15,7 @@ export const shorten = cacheFn(async (url: string) => {
 
 	await queue.promise;
 
-	const key = [...Array(KEY_LENGTH).keys()]
-		.map(() => chars[Math.floor(Math.random() * 1024) % chars.length])
-		.join('');
+	const key = await generateKey();
 
 	const shortLink = new URL(key, `https://${domain}`).href;
 
